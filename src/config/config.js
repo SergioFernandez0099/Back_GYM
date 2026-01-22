f51661d01676456dba6a16c365585aba26fe466b
SergioFernandez0099 <dialga.cuenco@gmail.com>
2026-01-22 02:08:29 +0100

    Mejoras de seguridad y history de ejercicios añadido

--- src/config/config.js
+++ src/config/config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 // Determinar qué archivo .env cargar según NODE_ENV
 // Cambiar en produccion
 const envFile = process.env.NODE_ENV === 'production'
-    ? '.env.production'
+    ? '.env'
     : '.env';
 
 // Cargar el archivo correspondiente
